4309a0433f8e24cbb41ef5d62727f9a642a8eac3
Lori Ta <lorijta@gmail.com>
2020-09-03 20:30:26 -0700

    query

---
+++ query.sql
@@ -0,0 +1,13 @@
+-- View product for account# 168
+SELECT ast.account_id, gpa.group_id, gpa.product_id
+FROM account_state_transitions AS ast
+JOIN accounts AS a ON ast.account_id = a.id
+	JOIN group_product_associations AS gpa ON gpa.group_id=a.group_id
+	JOIN products AS p ON gpa.product_id=p.id
+	WHERE ast.account_id=168;
+-- group 13, product 10, paid 106 ~ 9hrs
+	
+SELECT *
+FROM payments
+WHERE account_id = 168
+ORDER BY effective_when DESC;
