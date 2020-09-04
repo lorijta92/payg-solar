6b3fe414f0f1171657516de24854cddb99a88ed4
Lori Ta <lorijta@gmail.com>
2020-09-03 20:17:19 -0700

    schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,13 @@
--- acct_id: accounts, a_s_t, payments
+-- DROP TABLE IF EXISTS
+-- 	account_state_transitions,
+-- 	accounts,
+-- 	group_product_associations,
+-- 	groups,
+-- 	payments,
+-- 	products;
 
 CREATE TABLE account_state_transitions (
-    id BIGINT PRIMARY KEY,
+    id BIGINT,
     created_when TIMESTAMP,
     started_when TIMESTAMP,
     account_id INT,
@@ -11,7 +17,7 @@ CREATE TABLE account_state_transitions (
 );
 
 CREATE TABLE accounts (
-    id BIGINT PRIMARY KEY,
+    id BIGINT,
     group_id BIGINT,
     organization_id BIGINT,
     registration_date TIMESTAMP,
@@ -20,22 +26,22 @@ CREATE TABLE accounts (
 );
 
 CREATE TABLE group_product_associations (
-    id BIGINT PRIMARY KEY,
+    id BIGINT,
     group_id BIGINT,
     product_id BIGINT
 );
 
 CREATE TABLE groups (
-    id BIGINT PRIMARY KEY,
+    id BIGINT,
     name TEXT,
     price_upfront INT,
     price_unlock INT,
-    price_clock_hour INT,
-    minimum_payment INT
+    price_clock_hour DECIMAL,
+    minimum_payment DECIMAL
 );
 
 CREATE TABLE payments (
-    id BIGINT PRIMARY KEY,
+    id BIGINT,
     effective_when TIMESTAMP,
     account_id BIGINT,
     currency TEXT,
@@ -43,7 +49,7 @@ CREATE TABLE payments (
 );
 
 CREATE TABLE products (
-    id BIGINT PRIMARY KEY,
+    id BIGINT,
     name TEXT,
     type TEXT
 );
