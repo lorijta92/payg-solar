53d0f167f31a5583a0ed8677609aa7509bbbfacc
Lori Ta <lorijta@gmail.com>
2020-09-04 21:04:14 -0700

    to_state

--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
+-- Q1?
 -- View product for account# 168
 SELECT ast.account_id, gpa.group_id, gpa.product_id
 FROM account_state_transitions AS ast
@@ -11,3 +12,10 @@ SELECT *
 FROM payments
 WHERE account_id = 168
 ORDER BY effective_when DESC;
+-- end q1?
+
+-- START Q2
+SELECT started_when, account_id, from_state, to_state
+FROM account_state_transitions
+WHERE account_id=5
+ORDER BY started_when DESC;
