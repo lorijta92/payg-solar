fdd377fd6a3282c04bf76e86b5f597d95a89218a
Lori Ta <lorijta@gmail.com>
2020-09-07 19:35:03 -0700

    clean repo

--- part1_full.py
+++ part1_full.py
@@ -1,19 +1,19 @@
-# Full script for Part 1 (Q1-4)
+# Full script for Part 1
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import timedelta
 
 # Read in data
-accounts = pd.read_csv("Resources/accounts.csv") #P1
-gpa = pd.read_csv("Resources/group_product_associations.csv") #P1
-ast = pd.read_csv("Resources/account_state_transitions.csv") #P2 & P4
-payments = pd.read_csv("Resources/payments.csv") #P3
-accounts = pd.read_csv("Resources/accounts.csv") #P4
-groups = pd.read_csv("Resources/groups.csv") #P4
+accounts = pd.read_csv("Resources/accounts.csv") #1a
+gpa = pd.read_csv("Resources/group_product_associations.csv") #1a
+ast = pd.read_csv("Resources/account_state_transitions.csv") #1b & 1d
+payments = pd.read_csv("Resources/payments.csv") #1c
+accounts = pd.read_csv("Resources/accounts.csv") #1d
+groups = pd.read_csv("Resources/groups.csv") #1d
 
 
-### PART 1: VISUALIZE PRODUCT REGISTRATION ###
+### PART 1A: VISUALIZE PRODUCT REGISTRATION ###
 # Rename columns
 gpa = gpa.rename(columns={"id":"gpa_id"})
 accounts = accounts.rename(columns={"id":"account_id"})
@@ -67,7 +67,7 @@ label_bars(registration_rect2) # Call function
 plt.savefig("Output/product_registration2.png") # Save figure
 
 
-### MERGE DATAFRAMES FOR PART 4 ###
+### MERGE DATAFRAMES FOR PART 1D ###
 # Merge 'ast' and 'accounts' dataframes
 ast_accounts_merge = pd.merge(left=ast, right=accounts, how='outer', left_on='account_id', right_on='account_id')
 
@@ -81,27 +81,27 @@ ast_accounts_groups_merge = pd.merge(left=ast_accounts_merge, right=groups, how=
 aag_merge = ast_accounts_groups_merge.drop(columns=['id','name','price_clock_hour'])
 
 
-### SET TIMESTAMP FOR PARTS 2-4 ###
+### SET TIMESTAMP FOR PARTS 1C-1D ###
 timestamp_str = input("Enter date with YYYY-MM-DD format.") # Take date input
 timestamp_dt = pd.to_datetime(timestamp_str) # Convert string to datetime
 timestamp_dt = timestamp_dt + timedelta(hours=23,minutes=59,seconds=59) # Adjust timestamp to end of day
 
 
-### ADJUST DATA TYPE AND LIMIT DATAFRAME FOR PARTS 2-4 ###
+### ADJUST DATA TYPE AND LIMIT DATAFRAME FOR PARTS 1C-1D ###
 # Change data type from string to datetime object
-ast['started_when'] = pd.to_datetime(ast['started_when']) #P2
-payments['effective_when'] = pd.to_datetime(payments['effective_when']) #P3
-aag_merge['started_when'] = pd.to_datetime(aag_merge['started_when']) # P4
+ast['started_when'] = pd.to_datetime(ast['started_when']) #P1b
+payments['effective_when'] = pd.to_datetime(payments['effective_when']) #1c
+aag_merge['started_when'] = pd.to_datetime(aag_merge['started_when']) # 1d
 
 # Create limited dataframe based on indicated timestamp 
-ast_date_limited = ast.loc[ast['started_when'] <= timestamp_dt] #P2
-payments_date_limited = payments.loc[payments['effective_when'] <= timestamp_dt] #P3
+ast_date_limited = ast.loc[ast['started_when'] <= timestamp_dt] #1b
+payments_date_limited = payments.loc[payments['effective_when'] <= timestamp_dt] #1c
 
 # Also limit by 'to_state'
-aag_merge_date_limited = aag_merge.loc[(aag_merge['started_when'] <= timestamp_dt) & (aag_merge['to_state']=='DISABLED')] #P4
+aag_merge_date_limited = aag_merge.loc[(aag_merge['started_when'] <= timestamp_dt) & (aag_merge['to_state']=='DISABLED')] #1d
 
 
-### PART 2: TOTAL TIME IN STATE ###
+### PART 1B: TOTAL TIME IN STATE ###
 # Select max date for each unique account_id
 ast_filtered = ast_date_limited.loc[ast_date_limited.groupby('account_id')['started_when'].idxmax()]
 ast_filtered = ast_filtered[['started_when','account_id','to_state']] # Limit columns
@@ -112,7 +112,7 @@ time_in_state = [timestamp_dt - x for x in ast_filtered['started_when']]
 ast_filtered[f'time_in_state_as_of_{timestamp_str[:10]}'] = time_in_state # Add column to dataframe
 
 
-### PART 3: CUMULATIVE PAYMENTS ###
+### PART 1C: CUMULATIVE PAYMENTS ###
 currency = payments.currency[0] # Set currency
 
 # Group by account_id and perform aggregate functions
@@ -124,7 +124,7 @@ cumulative_payments = cumulative_payments.rename(columns={
     'amount':f'total_sum_payments_{currency}'})
 
 
-### PART 4: NOMINAL EXPECTED PAYMENTS ###
+### PART 1D: NOMINAL EXPECTED PAYMENTS ###
 # Group by account and count the occurrences of "DISABLED" state 
 aag_grouped_df = aag_merge_date_limited.groupby('account_id',as_index=False).agg({
     'to_state':'count',
@@ -145,6 +145,6 @@ expected_payments_df = pd.DataFrame({'account_id':aag_grouped_df['account_id'],
 
 # Export to csv
 print("FIND CHARTS AND TABLES IN OUTPUT DIRECTORY.")
-ast_filtered.to_csv(f"Output/time_in_state_as_of_{timestamp_str[:10]}.csv", index=False) #P2
-cumulative_payments.to_csv(f"Output/cumulative_payments_by_account_as_of_{timestamp_str[:10]}.csv", index=False) #P3
-expected_payments_df.to_csv(f"Output/nominal_expected_payments_as_of_{timestamp_str[:10]}.csv", index=False) # P4
+ast_filtered.to_csv(f"Output/time_in_state_as_of_{timestamp_str[:10]}.csv", index=False) #1b
+cumulative_payments.to_csv(f"Output/cumulative_payments_by_account_as_of_{timestamp_str[:10]}.csv", index=False) #1c
+expected_payments_df.to_csv(f"Output/nominal_expected_payments_as_of_{timestamp_str[:10]}.csv", index=False) #1d
